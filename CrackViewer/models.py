38d9418e18d92ab20f04f76d0cd943b3c7984af1
JinhaSong <thejn07@gmail.com>
2019-10-29 23:16:57 +0900

    classification result model, segmentation result model 추가

--- CrackViewer/models.py
+++ CrackViewer/models.py
@@ -11,3 +11,15 @@ class ImageModel(models.Model):
     severity_th = models.IntegerField(default=0)
     uploaded_date = models.DateTimeField(auto_now_add=True)
     # updated_date = models.DateTimeField(auto_now=True)
+
+class ClsResultModel(models.Model):
+    image = models.ForeignKey(ImageModel, related_name='cls_results', on_delete=models.CASCADE)
+    label = models.TextField(null=True, unique=False)
+    x = models.FloatField(null=True, unique=False)
+    y = models.FloatField(null=True, unique=False)
+    w = models.FloatField(null=True, unique=False)
+    h = models.FloatField(null=True, unique=False)
+
+class SegResultModel(models.Model):
+    image = models.ForeignKey(ImageModel, related_name='seg_results', on_delete=models.CASCADE)
+    seg_image = models.TextField(null=True, unique=False)
