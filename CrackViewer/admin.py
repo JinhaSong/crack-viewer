13ffc6de6963ef7d0c942f84ba956ae9af2cab75
JinhaSong <thejn07@gmail.com>
2019-10-29 23:18:36 +0900

    admin page에 ClsResultModel, SegResultModel 추가

--- CrackViewer/admin.py
+++ CrackViewer/admin.py
@@ -4,5 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from .models import ImageModel
+from .models import ClsResultModel
+from .models import SegResultModel
 
 admin.site.register(ImageModel)
+admin.site.register(ClsResultModel)
+admin.site.register(SegResultModel)
