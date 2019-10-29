6061fd496201152076087f58281b89146625e95d
JinhaSong <thejn07@gmail.com>
2019-10-30 02:27:00 +0900

    form을 통해 원본이미지(10000x3704)를 저장하고 crack-bridge-site에 분석 요청 후 분석 결과를 저장하도록 함수 수정 - classification 결과는 ClsResultModel에 label, x, y, w, h를 각각 저장 - segmentation 결과는 SegResultModel에 이미지(.png)를 저장

--- CrackViewer/views.py
+++ CrackViewer/views.py
@@ -18,6 +18,38 @@ def upload(request) :
 
         if form.is_valid():
             image = form.save()
+
+            # Read image path
+            image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../media/' ,str(image.image))
+
+            # Send analysis request to crack-bridge-site
+            analysis_request = AnalysisRequest()
+            b_image = analysis_request.load_binary_image(image_path)
+            analysis_request.set_request_attr(url='http://mltwins.sogang.ac.kr:8000/analyzer/', image=b_image, modules='crack')
+            response = json.loads((analysis_request.send_request_message().content).decode("utf-8"))['results'][0]['module_result']
+
+            # Get classification result and segmentation result from response of crack-bridge-site
+            cls_results = response['classification_result']
+            seg_result = response['image']
+
+            # Define model variables
+            segResultModel = SegResultModel.objects.create(image=image)
+
+            # Save classification result
+            for result in cls_results:
+                clsResultModel = ClsResultModel.objects.create(image=image)
+                clsResultModel.label = result['label'][0]['description']
+                clsResultModel.x = result['position']['x']
+                clsResultModel.y = result['position']['y']
+                clsResultModel.w = result['position']['w']
+                clsResultModel.h = result['position']['h']
+                clsResultModel.save()
+
+            # Save classification result
+            seg_img_path = os.path.join(settings.MEDIA_ROOT, str(image.image).split(".")[0] + "_seg" + ".png")
+            segResultModel.seg_image = ContentFile(base64.b64decode(seg_result), name=seg_img_path)
+            segResultModel.save()
+
             return redirect('/imagelist/')
         else :
             return redirect('/imagelist/')
