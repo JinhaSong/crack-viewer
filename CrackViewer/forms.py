adc4496fdc92b7bd50187cf8d9ea09855576b5c5
JinhaSong <thejn07@gmail.com>
2019-11-03 03:47:15 +0900

    SegGTUpload Form 추가

--- CrackViewer/forms.py
+++ CrackViewer/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import ImageModel
+from .models import SegGTModel
 
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImageModel
         fields = ['image']
+
+class SegGTUploadForm(forms.ModelForm):
+    class Meta:
+        model = SegGTModel
+        fields = ['seg_image']
