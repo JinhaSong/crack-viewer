ff0c1dfd56f0916dc30056b2ed32c36179874fea
JinhaSong <thejn07@gmail.com>
2019-11-03 03:46:46 +0900

    admin page에 SegGTModel, RegionResultModel, RegionPositionModel 추가

--- CrackViewer/admin.py
+++ CrackViewer/admin.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import ImageModel
-from .models import ClsResultModel
-from .models import SegResultModel
+from .models import *
 
 admin.site.register(ImageModel)
 admin.site.register(ClsResultModel)
 admin.site.register(SegResultModel)
+admin.site.register(SegGTModel)
+admin.site.register(RegionResultModel)
+admin.site.register(RegionPositionModel)
+
