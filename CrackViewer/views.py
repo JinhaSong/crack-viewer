9c95209a32e77256441626b8280443173c40fbe8
JinhaSong <thejn07@gmail.com>
2019-11-16 07:18:11 +0900

    분석 옵션, 보기 옵션 패널 추가 및 분석 버튼 추가 - upload 시 바로 분석을 하지 않고 imagedetail 페이지에서 분석하도록 수정 - analysis 관련 ajax 함수 및 view 함수 추가

--- CrackViewer/views.py
+++ CrackViewer/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseForbidden
 from django.core.files.base import ContentFile
 from django.views.decorators.csrf import csrf_exempt
-from .forms import ImageUploadForm, SegGTUploadForm
+from .forms import ImageGTUploadForm, ImageUploadForm
 from .models import *
 from .utils.AnalysisRequest import AnalysisRequest
 from CrackSite import settings
@@ -17,78 +17,10 @@ import json, os, base64
 @csrf_exempt
 def upload(request) :
     if request.method == "POST" :
-        form = ImageUploadForm(request.POST, request.FILES)
+        form = ImageGTUploadForm(request.POST, request.FILES)
 
         if form.is_valid():
-            image = form.save()
-
-            # Read image path
-            image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../media/' ,str(image.image))
-
-            # Send analysis request to crack-bridge-site
-            analysis_request = AnalysisRequest()
-            b_image = analysis_request.load_binary_image(image_path)
-            analysis_request.set_request_attr(url='http://mltwins.sogang.ac.kr:8000/analyzer/', image=b_image, modules='crack')
-            response = json.loads((analysis_request.send_request_message().content).decode("utf-8"))['results'][0]['module_result']
-
-            # Get classification result and segmentation result from response of crack-bridge-site
-            cls_results = response['classification_result']
-            seg_result = response['seg_image']
-            region_results = response['region_result']
-
-
-            # Save classification result
-            for result in cls_results:
-                clsResultModel = ClsResultModel.objects.create(image=image)
-
-                final_label = None
-                label_list = []
-                label_names = ['crack', 'lane', 'patch', 'normal']
-                for label in result['label'] :
-                    if label['description'] in label_names :
-                        label_list.append(label)
-                labels = sorted(label_list, key=lambda label_list:(label_list['score']), reverse=True)
-                final_label = labels[0]['description']
-
-                if labels[0]['description'] == 'crack':
-                    label_list = []
-                    detail_label_names = ['lc', 'tc', 'ac', 'detail_norm']
-                    for label in result['label']:
-                        if label['description'] in detail_label_names:
-                            label_list.append(label)
-                    labels = sorted(label_list, key=lambda label_list: (label_list['score']), reverse=True)
-                    if labels[0]['description'] == 'detail_norm' :
-                        final_label = 'normal'
-                    else :
-                        final_label = labels[0]['description']
-
-                clsResultModel.label = final_label
-                clsResultModel.x = result['position']['x']
-                clsResultModel.y = result['position']['y']
-                clsResultModel.w = result['position']['w']
-                clsResultModel.h = result['position']['h']
-                clsResultModel.save()
-
-            for region in region_results :
-                region_area = region['region_area']
-                regionResultModel = RegionResultModel.objects.create(image=image)
-                regionResultModel.region_num = region['region']
-                regionResultModel.region_type = region['region_type']
-                for patch in region_area :
-                    regionPositionModel = RegionPositionModel.objects.create(region_model=regionResultModel)
-                    regionPositionModel.x = patch['x']
-                    regionPositionModel.y = patch['y']
-                    regionPositionModel.w = patch['w']
-                    regionPositionModel.h = patch['h']
-                    regionPositionModel.save()
-                regionResultModel.save()
-
-
-            # Save segmentation result
-            segResultModel = SegResultModel.objects.create(image=image)
-            seg_img_path = os.path.join(str(image.image).split(".")[0] + "_seg" + ".png")
-            segResultModel.seg_image = ContentFile(base64.b64decode(seg_result), name=seg_img_path)
-            segResultModel.save()
+            form.save()
 
             return redirect('/imagelist/')
         else :
@@ -97,35 +29,17 @@ def upload(request) :
         template_name = "upload.html"
         return render(request, template_name)
 
-def compare_seg(request, image_pk) :
-    if request.method == "POST":
-        form = SegGTUploadForm(request.POST, request.FILES)
-        print("form", form)
+@csrf_exempt
+def upload_without_gt(request) :
+    if request.method == "POST" :
+        form = ImageUploadForm(request.POST, request.FILES)
+
         if form.is_valid():
-            print("valid")
-            seg_image = form.save(commit=False)
-            seg_image.image = ImageModel.objects.get(pk=image_pk)
-            seg_image.save()
-            return redirect('/compare_seg/' + str(image_pk))
-
+            form.save()
+            return redirect('/imagelist/')
         else :
-            print('invalid')
-            return redirect('/compare_seg/' + str(image_pk))
-    else:
-        template_name = "compare_seg.html"
-        image = ImageModel.objects.filter(pk=image_pk)
-        seg_result = SegResultModel.objects.filter(image=image_pk)
-        seg_gt = SegGTModel.objects.filter(image=image_pk)
-        is_seg_gt = None
+            return redirect('/imagelist/')
 
-        if len(seg_gt) > 0:
-            is_seg_gt = True
-            seg_gt = seg_gt[0]
-        else:
-            is_seg_gt = False
-        return render(request, template_name, {
-            'image': image[0], 'image_pk': image_pk, 'seg_result':seg_result[0], 'seg_gt': seg_gt, 'is_seg_gt':is_seg_gt
-        })
 
 def image_list(request) :
     image_model = ImageModel.objects.all().order_by('-pk')[:10]
@@ -135,13 +49,25 @@ def image_list(request) :
 def image_detail(request, image_pk) :
     image = ImageModel.objects.filter(pk=image_pk)
     seg_result = SegResultModel.objects.filter(image=image_pk)
-    seg_image_url = str(seg_result[0].seg_image)
+    seg_gt = SegGTModel.objects.filter(image=image_pk)
+    is_analyzed = False
+    is_gt = False
+    seg_image_url = ""
+    if len(seg_result) > 0 :
+        is_analyzed = True
+        seg_image_url = str(seg_result[0].seg_image)
+    if len(seg_gt) > 0 :
+        is_gt = True
+
 
     return render(request, 'imagedetail.html', {
-                      'image': image[0],
-                      'image_pk': image_pk,
-                      'seg_result': seg_image_url,
-                  })
+                    'image': image[0],
+                    'image_pk': image_pk,
+                    'seg_result': seg_image_url,
+                    'seg_gt':seg_gt,
+                    'is_analyzed': is_analyzed,
+                    'is_gt': is_gt
+                })
 @csrf_exempt
 def get_cracks(request) :
     cracks = []
@@ -167,14 +93,122 @@ def get_regions(request) :
             dict_region['region_num'] = region.region_num
             dict_region['region_type'] = region.region_type
             dict_region['patchs'] = []
+            dict_region['distress_width'] = region.distress_width
+            dict_region['distress_height'] = region.distress_height
+            dict_region['distress_area'] = region.distress_area
+            dict_region['distress_serverity'] = region.distress_serverity
             patchs = RegionPositionModel.objects.filter(region_model=region)
             for patch in patchs:
                 dict_patch = {}
+                dict_patch['label'] = patch.label
                 dict_patch['x'] = patch.x
                 dict_patch['y'] = patch.y
                 dict_patch['w'] = patch.w
                 dict_patch['h'] = patch.h
                 dict_region['patchs'].append(dict_patch)
-
             regions.append(dict_region)
     return HttpResponse(json.dumps({"regions": regions}), 'application/json')
+
+@csrf_exempt
+def analysis(request) :
+    result = []
+    if request.method == "POST" :
+        image = ImageModel.objects.filter(pk=request.POST['image_pk'])[0]
+        image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../media/', str(image.image))
+
+        # Send analysis request to crack-bridge-site
+        analysis_request = AnalysisRequest()
+        b_image = analysis_request.load_binary_image(image_path)
+        analysis_request.set_request_attr(
+            url='http://mltwins.sogang.ac.kr:8000/analyzer/',
+            image=b_image, modules='crack',
+            patch_size=request.POST['patch_size'],
+            region_connectivity=int(request.POST['region_connectivity']),
+            region_noise_filter=int(request.POST['region_noise_filter']),
+            severity_threshold=int(request.POST['severity_threshold']),
+        )
+        response = json.loads((analysis_request.send_request_message().content).decode("utf-8"))['results'][0]['module_result']
+
+        # Get classification result and segmentation result from response of crack-bridge-site
+        cls_result = response['cls_result']
+        seg_result = response['seg_image']
+        region_results = response['region_result']
+
+        for result in cls_result :
+            clsResultModel = ClsResultModel.objects.create(image=image)
+
+            final_label = ''
+            label_list = []
+            label_names = ['crack', 'lane', 'patch', 'normal']
+            for label in result['label']:
+                if label['description'] in label_names:
+                    label_list.append(label)
+            labels = sorted(label_list, key=lambda label_list: (label_list['score']), reverse=True)
+            final_label = labels[0]['description']
+
+            if labels[0]['description'] == 'crack':
+                label_list = []
+                detail_label_names = ['lc', 'tc', 'ac', 'detail_norm']
+                for label in result['label']:
+                    if label['description'] in detail_label_names:
+                        label_list.append(label)
+                labels = sorted(label_list, key=lambda label_list: (label_list['score']), reverse=True)
+                if labels[0]['description'] == 'detail_norm':
+                    final_label = 'normal'
+                else:
+                    final_label = labels[0]['description']
+
+            clsResultModel.label = final_label
+            clsResultModel.x = result['position']['x']
+            clsResultModel.y = result['position']['y']
+            clsResultModel.w = result['position']['w']
+            clsResultModel.h = result['position']['h']
+            clsResultModel.save()
+
+        print(region_results)
+        for region in region_results :
+            region_area = region['region_area']
+            regionResultModel = RegionResultModel.objects.create(image=image)
+            regionResultModel.region_num = region['region']
+            regionResultModel.region_type = region['region_type']
+
+            # if region['distress_width'] == "null" :
+            #     regionResultModel.distress_width = None
+            # else :
+            #     regionResultModel.distress_width = region['distress_width']
+            #
+            # if region['distress_height'] == "null":
+            #     regionResultModel.distress_height = None
+            # else :
+            #     regionResultModel.distress_height = region['distress_height']
+            #
+            # if region['distress_area'] == "null":
+            #     regionResultModel.distress_area = None
+            # else :
+            #     regionResultModel.distress_area = region['distress_area']
+            # if region['distress_serverity'] == "null":
+            #     regionResultModel.distress_serverity = None
+            # else :
+            #     regionResultModel.distress_serverity = region['distress_serverity']
+
+
+            for patch in region_area :
+                regionPositionModel = RegionPositionModel.objects.create(region_model=regionResultModel)
+
+
+                regionPositionModel.x = patch['x']
+                regionPositionModel.y = patch['y']
+                regionPositionModel.w = patch['w']
+                regionPositionModel.h = patch['h']
+                regionPositionModel.save()
+            regionResultModel.save()
+
+
+        # Save segmentation result
+        segResultModel = SegResultModel.objects.create(image=image)
+        seg_img_path = os.path.join(str(image.image).split(".")[0] + "_seg" + ".png")
+        segResultModel.seg_image = ContentFile(base64.b64decode(seg_result), name=seg_img_path)
+        segResultModel.save()
+        return HttpResponse(json.dumps({"state": True}), 'application/json')
+    else:
+        return HttpResponse(json.dumps({"state": False}), 'application/json')
