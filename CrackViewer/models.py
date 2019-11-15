934f3375f62c7355b8f64a5f0c9ca969baea5527
JinhaSong <thejn07@gmail.com>
2019-11-16 03:37:25 +0900

    model 및 form 수정

--- CrackViewer/models.py
+++ CrackViewer/models.py
@@ -6,14 +6,14 @@ from CrackViewer.utils import filename
 
 class ImageModel(models.Model):
     image = models.ImageField(upload_to=filename.default)
+    seg_gt_image = models.ImageField(upload_to=filename.default, null=True)
     token = models.AutoField(primary_key=True)
     cc_th = models.IntegerField(default=0)
     severity_th = models.IntegerField(default=0)
     uploaded_date = models.DateTimeField(auto_now_add=True)
-    # updated_date = models.DateTimeField(auto_now=True)
 
 class ClsResultModel(models.Model):
-    image = models.ForeignKey(ImageModel, related_name='cls_results', on_delete=models.CASCADE)
+    image = models.ForeignKey(ImageModel, related_name='crack_reulst', on_delete=models.CASCADE)
     label = models.TextField(null=True, unique=False)
     x = models.FloatField(null=True, unique=False)
     y = models.FloatField(null=True, unique=False)
@@ -27,16 +27,25 @@ class SegResultModel(models.Model):
 class SegGTModel(models.Model):
     image = models.ForeignKey(ImageModel, related_name='seg_gt', on_delete=models.CASCADE)
     seg_image = models.ImageField()
+    tp_image = models.ImageField(null=True)
+    fp_image = models.ImageField(null=True)
+    fn_image = models.ImageField(null=True)
 
 class RegionResultModel(models.Model):
     image = models.ForeignKey(ImageModel, related_name='region_result', on_delete=models.CASCADE)
     region_num = models.IntegerField(null=True, unique=False)
     region_type = models.TextField(null=True, unique=False)
+    distress_width = models.FloatField(null=True, unique=False)
+    distress_height = models.FloatField(null=True, unique=False)
+    distress_area = models.FloatField(null=True, unique=False)
+    distress_serverity = models.TextField(null=True, unique=False)
 
 
 class RegionPositionModel(models.Model):
     region_model = models.ForeignKey(RegionResultModel, related_name='region_positions', on_delete=models.CASCADE)
+    label = models.TextField(null=True, unique=False)
     x = models.FloatField(null=True, unique=False)
     y = models.FloatField(null=True, unique=False)
     w = models.FloatField(null=True, unique=False)
     h = models.FloatField(null=True, unique=False)
+
