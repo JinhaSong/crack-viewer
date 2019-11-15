27ad20c755053b0ae2d02ce198e1112f16002022
JinhaSong <thejn07@gmail.com>
2019-11-16 07:14:04 +0900

    main url을 imagelist로 변경

--- CrackSite/urls.py
+++ CrackSite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from CrackViewer import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('CrackViewer.urls')),
+    url(r'^$', views.image_list, name='image_list')
 ]
