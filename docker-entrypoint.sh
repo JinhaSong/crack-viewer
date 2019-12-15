9fd9ae8b389d57fc86a1d8d38221e4afe2ab33df
JinhaSong <thejn07@gmail.com>
2019-12-15 23:16:45 +0900

    docker-entrypoint trap 추가

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,4 +13,6 @@ if not get_user_model().objects.filter(username='$DJANGO_SUPERUSER_USERNAME'):
     get_user_model()._default_manager.db_manager().create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')"
 sh server_start.sh
 
+trap 'sh server_shutdown.sh' EXIT
+
 exec "$@"
